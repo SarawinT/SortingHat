bec6467ba89473c11a7c667fdf2a028166163d0d
SarawinT <contact.sarawin@gmail.com>
2023-02-26 18:33:10 +0700

    Init Commit

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = process.env.PORT || 80;
+
+app.use(express.static(path.join(__dirname, '/www/')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '/www/index.html'));
+});
+
+app.listen(port);
+console.log('Server started at http://localhost:' + port);
